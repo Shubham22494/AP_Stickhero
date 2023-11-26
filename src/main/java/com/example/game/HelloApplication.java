dcc8a55983c756ea8a96a30e343defe831edb9ed
shubh <shubham22494@iiitd.ac.in>
2023-11-27 02:16:06 +0530

    Game

--- src/main/java/com/example/game/HelloApplication.java
+++ src/main/java/com/example/game/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.game;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +12,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();
+        HelloController controller = fxmlLoader.getController(); // Corrected variable name
+        Scene scene = new Scene(root);
+        stage.setTitle("Stick Hero Game(Invisible Button to increase stick press on or above character )");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/game/HelloController.java
+++ src/main/java/com/example/game/HelloController.java
@@ -1,14 +1,65 @@
 package com.example.game;
 
+
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Rectangle stick;
+    @FXML
+    private ImageView cartoonCharacter;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void incresestick(ActionEvent event) {
+        handleStickChange();
+    }
+
+    @FXML
+    protected void incresestick(MouseEvent event) {
+        handleStickChange();
+    }
+    @FXML
+    protected void rotate(MouseEvent e){
+        stick.setRotate(0);
+        stick.setTranslateY(stick.getHeight()/2.0);  // Adjust the translation
+        stick.setTranslateX(stick.getHeight()/2.0 );
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(1), stick);
+        rotateTransition.setByAngle(90);
+        rotateTransition.play();
+        animateCartoonCharacter();
+
+    }
+    private void handleStickChange() {
+        double currentHeight = stick.getHeight();
+        double newHeight = currentHeight + 10.0;
+
+        // Adjust the layoutY to maintain the bottom anchor point
+        stick.setLayoutY(stick.getLayoutY() - (newHeight - currentHeight));
+
+        // Increase the stick height
+        stick.setHeight(newHeight);
+    }
+    private void animateCartoonCharacter() {
+        // Create a TranslateTransition for the cartoon character
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(2), cartoonCharacter);
+
+        // Set the movement along the X-axis
+        translateTransition.setByX(stick.getHeight());
+
+        // Set the cycle count to indefinite for continuous animation
+        translateTransition.setCycleCount(1);
+
+        // Set auto-reverse to true for back-and-forth movement
+//        translateTransition.setAutoReverse(true);
+
+        // Start the animation
+        translateTransition.play();
     }
 }

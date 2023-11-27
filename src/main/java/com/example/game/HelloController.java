abe29ba3ffe9b1cce5e901140b14f35c25e9afda
shubh <shubham22494@iiitd.ac.in>
2023-11-27 20:14:38 +0530

    Game

--- src/main/java/com/example/game/HelloController.java
+++ src/main/java/com/example/game/HelloController.java
@@ -1,6 +1,6 @@
 package com.example.game;
 
-
+import javafx.animation.PauseTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
@@ -11,55 +11,86 @@ import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class HelloController {
+    double lenght;
     @FXML
     private Rectangle stick;
     @FXML
+    private Rectangle pillar1;
+    @FXML
+    private Rectangle pillar2;
+    @FXML
     private ImageView cartoonCharacter;
 
+    private boolean buttonPressed = false;
+
     @FXML
-    protected void incresestick(ActionEvent event) {
-        handleStickChange();
+    protected void incresestickPressed(MouseEvent eve) {
+        buttonPressed = true;
+        incresestick();
     }
 
     @FXML
-    protected void incresestick(MouseEvent event) {
-        handleStickChange();
+    protected void incresestickReleased(MouseEvent ee) {
+        buttonPressed = false;
+        incresestick();
     }
+
     @FXML
-    protected void rotate(MouseEvent e){
+    protected void incresestick() {
+        if (buttonPressed) {
+            increaseStick();
+        }
+        else{
+            rotate();
+        }
+    }
+
+    @FXML
+    protected void rotate() {
         stick.setRotate(0);
-        stick.setTranslateY(stick.getHeight()/2.0);  // Adjust the translation
-        stick.setTranslateX(stick.getHeight()/2.0 );
+        stick.setTranslateY(stick.getHeight() / 2.0);
+        stick.setTranslateX(stick.getHeight() / 2.0);
         RotateTransition rotateTransition = new RotateTransition(Duration.seconds(1), stick);
         rotateTransition.setByAngle(90);
         rotateTransition.play();
         animateCartoonCharacter();
-
     }
-    private void handleStickChange() {
+
+    private void increaseStick() {
         double currentHeight = stick.getHeight();
-        double newHeight = currentHeight + 10.0;
-
-        // Adjust the layoutY to maintain the bottom anchor point
+        double newHeight = currentHeight + 1.0;
+        lenght+=1.0;
         stick.setLayoutY(stick.getLayoutY() - (newHeight - currentHeight));
-
-        // Increase the stick height
         stick.setHeight(newHeight);
+
+        // Add a pause to control the speed of height increase
+        addDelay(0.02); // You can adjust the duration as needed
     }
+
     private void animateCartoonCharacter() {
-        // Create a TranslateTransition for the cartoon character
         TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(2), cartoonCharacter);
-
-        // Set the movement along the X-axis
         translateTransition.setByX(stick.getHeight());
-
-        // Set the cycle count to indefinite for continuous animation
         translateTransition.setCycleCount(1);
-
-        // Set auto-reverse to true for back-and-forth movement
-//        translateTransition.setAutoReverse(true);
-
-        // Start the animation
         translateTransition.play();
+        if(lenght>=(pillar2.getX()-pillar1.getX())){
+            translateTransition.setByX(pillar2.getX());
+            translateTransition.play();
+        }
+        else{
+            translateTransition.setByY(0);
+            translateTransition.play();
+        }
+
     }
+
+    private void addDelay(double seconds) {
+        PauseTransition delay = new PauseTransition(Duration.seconds(seconds));
+        delay.setOnFinished(event -> {
+            if (buttonPressed) {
+                increaseStick(); // Continue increasing only if the button is still pressed
+            }
+        });
+        delay.play();
+    }
+
 }
